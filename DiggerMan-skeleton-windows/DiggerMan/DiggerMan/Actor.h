01e6328176915f10b5b8e09b11c9227716ccb96c
SA3154 <476315534@qq.com>
2017-05-05 15:25:18 -0700

    some changes
    there are still some errors. I am still working on it.

--- DiggerMan-skeleton-windows/DiggerMan/DiggerMan/Actor.h
+++ DiggerMan-skeleton-windows/DiggerMan/DiggerMan/Actor.h
@@ -2,44 +2,49 @@
 #define ACTOR_H_
 
 #include "GraphObject.h"
-class StudentWorld; 
-
+class StudentWorld;
 class Actor : public GraphObject
 {
 private:
-	StudentWorld * m_SW;
+	bool isAlive;
+	StudentWorld *world;
 public:
-	Actor(int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0);
+	Actor(StudentWorld *p, int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0);
+	void setAlive(bool Is);
+	bool getAlive();
+	StudentWorld * getWorld();
 	virtual ~Actor();
-	
-	StudentWorld* getWorld();
 	virtual void doSomething() = 0;
 
 };
 class Dirt : public Actor
 {
+private:
 public:
-	Dirt(int imageID, int startX, int startY, Direction dir=right, double size = .25, unsigned int depth = 3);
-
-	
-	
+	Dirt(StudentWorld *p, int imageID, int startX, int startY, Direction dir = right, double size = .25, unsigned int depth = 3);
 	void doSomething();
-
 	~Dirt();
 };
 
 class DiggerMan : public Actor
 {
+private:
+	int life;
+	int health;
 public:
-	DiggerMan(int imageID, int startX, int startY, Direction dir=right, double size = 1.0, unsigned int depth = 0);
-
+	DiggerMan(StudentWorld *p,int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0);
 	void doSomething();
-
-	
 
 	~DiggerMan();
 };
-
+class Boulders : public Actor
+{
+private:
+public:
+	Boulders(StudentWorld *p, int imageID, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 1);
+	void doSomething();
+	~Boulders();
+};
 
 
 
