42f41df04c575afa66575c14c041eb36f4f3cf01
SA3154 <476315534@qq.com>
2017-05-15 19:49:17 -0700

    Update StudentWorld.h

--- DiggerMan-skeleton-windows/DiggerMan/DiggerMan/StudentWorld.h
+++ DiggerMan-skeleton-windows/DiggerMan/DiggerMan/StudentWorld.h
@@ -3,24 +3,19 @@
 
 #include "GameWorld.h"
 #include "GameConstants.h"
+#include "Actor.h"
 #include <string>
 #include <vector>
 using namespace std;
-
-class Actor;
-class Dirt;
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
-
 class StudentWorld : public GameWorld
 {
 private:
-
-public:
-    
-    Actor* m_diggerman;
-    Dirt* dirtarr[64][64];
+	Actor* m_diggerman;
+	Dirt* dirtarr[64][60];
 	vector<Actor*> actors;
-    
+public:
+
 	StudentWorld(std::string assetDir)
 		: GameWorld(assetDir)
 	{
@@ -31,8 +26,8 @@ public:
 	virtual int move();
 
 	virtual void cleanUp();
-
 	
+	void removeDirt();
 
 };
 
